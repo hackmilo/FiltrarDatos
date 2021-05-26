826915886aeaa57bdd735d82c7acd13d8f05fd49
Andrés Rozo <80934853+hackmilo@users.noreply.github.com>
2021-05-26 11:41:45 -0500

    Filtrar datos de una lista con diccionarios.
    En Branch 1 "Main" ejercicio para filtrar datos utilizando List comprehensions.

---
+++ filtrando_datos.py
@@ -0,0 +1,86 @@
+DATA = [ #al estar en mayusculas no es variable sino constante
+    {
+        'name': 'Facundo',
+        'age': 72,
+        'organization': 'Platzi',
+        'position': 'Technical Coach',
+        'language': 'python',
+    },
+    {
+        'name': 'Luisana',
+        'age': 33,
+        'organization': 'Globant',
+        'position': 'UX Designer',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Héctor',
+        'age': 19,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'ruby',
+    },
+    {
+        'name': 'Gabriel',
+        'age': 20,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Isabella',
+        'age': 30,
+        'organization': 'Platzi',
+        'position': 'QA Manager',
+        'language': 'java',
+    },
+    {
+        'name': 'Karo',
+        'age': 23,
+        'organization': 'Everis',
+        'position': 'Backend Developer',
+        'language': 'python',
+    },
+    {
+        'name': 'Ariel',
+        'age': 32,
+        'organization': 'Rappi',
+        'position': 'Support',
+        'language': '',
+    },
+    {
+        'name': 'Juan',
+        'age': 17,
+        'organization': '',
+        'position': 'Student',
+        'language': 'go',
+    },
+    {
+        'name': 'Pablo',
+        'age': 32,
+        'organization': 'Master',
+        'position': 'Human Resources Manager',
+        'language': 'python',
+    },
+    {
+        'name': 'Lorena',
+        'age': 56,
+        'organization': 'Python Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    },
+]
+
+
+def run():
+    all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
+    all_Platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
+    adults = [worker['name'] for worker in DATA if worker['age'] > 18]
+    old_confirmation = lambda worker_age: worker_age > 70
+    old_people = [worker | {'old': old_confirmation(worker['age'])} for worker in DATA]
+
+    for worker in old_people:
+        print(worker)  
+
+if __name__ == '__main__':
+    run()
