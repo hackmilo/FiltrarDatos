d51d95c57340a176a0845ab0befa4ded869b239e
Andrés Rozo <80934853+hackmilo@users.noreply.github.com>
2021-05-26 11:48:54 -0500

    Update filtrando_datos.py

--- filtrando_datos.py
+++ filtrando_datos.py
@@ -73,11 +73,13 @@ DATA = [ #al estar en mayusculas no es variable sino constante
 
 
 def run():
-    all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
-    all_Platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
-    adults = [worker['name'] for worker in DATA if worker['age'] > 18]
-    old_confirmation = lambda worker_age: worker_age > 70
-    old_people = [worker | {'old': old_confirmation(worker['age'])} for worker in DATA]
+    all_python_devs = list(filter(lambda worker: worker['language'] == 'python', DATA))
+    all_python_devs = list(map(lambda worker: worker['name'], all_python_devs))
+    all_platzi_workers = list(filter(lambda worker: worker['organization'] == 'Platzi', DATA))
+    all_platzi_workers = list(map(lambda worker: worker['name'], all_platzi_workers))
+    adults = list(filter(lambda worker: worker["age"] > 18, DATA))
+    adults = list(map(lambda worker: worker["name"], adults))
+    old_people = list(map(lambda worker: worker | {"old": worker["age"] > 70}, DATA))
 
     for worker in old_people:
         print(worker)  
